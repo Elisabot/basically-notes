7978c4683aa434a9fefee21b88890fce88370ce0
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-07-20 12:45:15 -0700

    benefits of .join over .toString

---
+++ arrayToString.js
@@ -0,0 +1,13 @@
+// say something with chars in an array
+const arrOfChars = ['B', 'e','h', 'i', 'n', 'd', ' ', 'y', 'o', 'u']
+
+// turn that array into string
+
+// meh this includes commas
+console.log(arrOfChars.toString())
+
+// cool, join gives you options for what to insert
+console.log(arrOfChars.join(''))
+// if you leave no arg it comma seperates
+// otherwise you can put whatever you want in there
+console.log(arrOfChars.join('potato'))
