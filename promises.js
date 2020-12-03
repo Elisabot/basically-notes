c6ec55ab6de696f231eab00ebfac1ee073ce6f9b
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-12-03 13:28:39 -0800

    wondering about how verbose a promise can get, time for lunch tho

--- promises.js
+++ promises.js
@@ -1,10 +1,10 @@
 // as many times as it takes, love
-
+let condition = true
 // what's one look like?
 
 const coolPromiseBro = (value) => { 
   return new Promise ((resolve, reject) => {
-      if (value === logicStuff) {
+      if (condition) {
         resolve('action or data may be specified here')
       } else {
         reject('action or data may be specified here')
@@ -33,3 +33,14 @@ const executor = (resolve, reject) => {
 
 const passDisPromiseAnExecutor = new Promise (executor);
 // you don't even need to slap a handler on it
+
+// OR you could work with everything happening in place
+const extraChunkyPromiseInPlace = () => { 
+  return new Promise ((resolve, reject) => {
+    if (condition) {
+      resolve('action or data, can we do a handler in here?')
+    } else {
+      reject('action or data, can we do a handler in here?')
+    }
+  })
+}
