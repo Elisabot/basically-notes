f58fad9aa8597cad37149e6c191d8a14704467a5
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-12-08 18:32:50 -0800

    async await basics

---
+++ async.js
@@ -0,0 +1,21 @@
+// looks like
+
+async function () {
+  // do stuff
+}
+
+const someFunc = async () => {
+  if (condition) {
+    return 'possitive resolution'
+  } else {
+    return 'err, my friend'
+  }
+}
+
+// three possible outcomes
+// undefined, a non-promise resolved value, or a promise
+
+// await is an operator used within async functions
+// that halts or pauses execution of the async function
+// until a given promise is resolved
+
