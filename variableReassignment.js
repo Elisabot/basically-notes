a07c08697cb41c12618417a9e5fecb75d56d219b
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-12-08 18:44:31 -0800

    now with more salt and vinegar!

--- variableReassignment.js
+++ variableReassignment.js
@@ -5,27 +5,29 @@
 let name = 'Bernie'
 
 // our method that just happens to reassign
+// bc we forgot a let or const in there
+// so name is gonna be acting on that global
 function sayName(arg) {
 	name = arg
 	console.log(name)
 }
 
+console.log(name)
 // should Bernie
-console.log(name)
-// should Earnie
-sayName('Earnie')
-// uh oh still Earnie!
-console.log(name)
 
+sayName('Earnie')
+// BAM! reassigned your global by calling your method
+console.log(name)
+// see?? still Earnie!
 
 // THIS CAN ALL BE PREVENTED 
-// CREATE A LOCAL VARIABLE IN YER METHOD <3
+// CREATE A LOCAL VARIABLE <3
 function sayMyName(arg) {
 	let name = arg
 	console.log(name)
 }
 
-// should Mernie
 sayMyName('Mernie')
-// should Earnie
+// should Mernie
 console.log(name)
+// should Earnie
